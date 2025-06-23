a2cf4a6b0aae9dd37cb165413124b34374ed7623
Felipe Barros <barrosfelipe735@gmail.com>
2025-06-23 19:52:07 -0300

    Criando a página de mostrar receitas

---
+++ src/components/CardReceitas.vue
@@ -0,0 +1,53 @@
+<template>
+  <article class="receitas_conteudo">
+    <header>
+      <img :src="'/public/imagens/receitas/' + receitasListadas.imagem" alt="" />
+    </header>
+
+    <section class="receita_corpo">
+      <h1 class="paragrafo receita_nome">{{ receitasListadas.nome }}</h1>
+    </section>
+  </article>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue'
+import type IReceitas from '@/interfaces/IReceitas.ts'
+export default {
+  props: {
+    receitasListadas: {
+      type: Object as PropType<IReceitas>,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.receitas_conteudo {
+  display: flex;
+  width: 14.6rem;
+  max-width: 19.5rem;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 1rem;
+  background: var(--Branco, #FFF);
+  box-shadow: 4px 4px 12px 0px rgba(68, 68, 68, 0.08)
+}
+
+.receitas_conteudo img {
+  width: 100%;
+  border-radius: 1rem 1rem 0rem 0rem;
+  display: block;
+}
+
+.receita_corpo {
+  padding: 2rem 1rem;
+}
+
+.receita_nome {
+  font-weight: 700;
+  color: var(--cinza, #444);
+  text-align: center;
+}
+</style>
--- src/components/MostrarReceitas.vue
+++ src/components/MostrarReceitas.vue
@@ -1,10 +1,40 @@
 <template>
-    TESTE....
+    <div class="receitas">
+        <div v-for="receita in receitas" :key="receita.nome">
+            <card-receitas :receitasListadas="receita" />
+        </div>
+    </div>
+
+    <BotaoPrincipal :textoBotao="textoReceitas"/>
 </template>
 
 <script lang="ts">
+import CardReceitas from './CardReceitas.vue';
+import { pegaReceitas } from '@/http/receitas.ts'
+import type IReceitas from '@/interfaces/IReceitas.ts'
+import BotaoPrincipal from './BotaoPrincipal.vue';
+export default {
+    components: { CardReceitas, BotaoPrincipal },
+    data() {
+        return {
+            receitas: [] as IReceitas,
+            textoReceitas: 'Editar Lista'
+        }
+    },
+
+    async created() {
+        this.receitas = await pegaReceitas()
+        console.log(this.receitas);
+    }
+}
 </script>
 
 <style scoped>
-
+.receitas {
+    margin-bottom: 1rem;
+    display: flex;
+    justify-content: center;
+    gap: 1.5rem;
+    flex-wrap: wrap;
+}
 </style>
